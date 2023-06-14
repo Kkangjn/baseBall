fc2694bfb27c30be7dd48ed6289254a619ff5437
Kkangjn <135511684+Kkangjn@users.noreply.github.com>
2023-06-14 17:33:53 +0900

    완성!

--- Baseball.java
+++ Baseball.java
@@ -2,81 +2,36 @@ import java.util.*;
 
 public class Baseball {
     public static void main(String[] args) {
-        System.out.println("컴퓨터가 랜덤숫자를 생성합니다.");
-        Random random = new Random();
-        ArrayList<Integer> computerNumList = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        int selectNum;
 
-        ArrayList<Integer> randomNumber = new ArrayList<Integer>();
-        for (int i=0; i<=9; i++) {
-            randomNumber.add(i);
-        }
+        do {
+            System.out.println("3~9 사이 값을 입력하세요!");
+            System.out.print("숫자 입력 :");
+            selectNum = sc.nextInt();
+            if(selectNum<3 || selectNum>9){
+                System.out.println("범위를 초과했습니다.");
+            }
+        }while(selectNum<3 || selectNum>9);
 
-        for (int i=0;i<=2;){
-            int j = 10-i;
-            int randomNum = random.nextInt(j);
-            computerNumList.add(randomNumber.get(randomNum));
-            randomNumber.remove(randomNum);
-            i++;
-        }
-        System.out.println(computerNumList.toString());
+        Random_Number randomNumber = new Random_Number(selectNum);
+        ArrayList<Integer> computerNumList = randomNumber.createNum();
         System.out.println("컴퓨터가 숫자 생성 완료! 답을 맞춰보세요!");
+        int tryNum = 0;
+        String result;
 
+        do {
+            User_Number userNumber = new User_Number(selectNum);
+            ArrayList<Integer> userNumList = userNumber.inputNum();
 
+            System.out.println(tryNum+1 + "번째 시도: " + userNumList.toString());
+            Compare compareNum = new Compare();
+            result = compareNum.compareNum(computerNumList, userNumList);
+            tryNum++;
+        }while(!(Objects.equals(result, selectNum+"S")));
+        System.out.println(tryNum + "번만에 맞히셨습니다!");
+        System.out.println("게임을 종료합니다.");
 
-        replay:
-        for (int x=1;x>=0;x++) {
-            System.out.println("숫자를 입력하세요");
-            Scanner sc = new Scanner(System.in);
-            ArrayList<Integer> userNumList = new ArrayList<Integer>();
-            createUserNum:
-            for (int i = 1; i <= computerNumList.size(); ) {
-                System.out.println(i+"번째 숫자를 입력하세요");
-                int userNum = sc.nextInt();
-
-                if (userNum>=10){
-                    System.out.println("잘못된 입력입니다.");
-                    continue createUserNum;
-                }
-
-                for (int j : userNumList) {
-                    if (j == userNum) {
-                        System.out.println("중복된 값입니다.");
-                        continue createUserNum;
-                    }
-                }
-                userNumList.add(userNum);
-                i++;
-            }
-//            System.out.println(userNumList.toString());
-
-            System.out.println(x+"번째 시도: "+userNumList.toString());
-            int strike = 0;
-            int ball = 0;
-            for (int i = 0; i < userNumList.size(); i++) {
-                int checkUserNum = userNumList.get(i);
-                for (int j = 0; j < computerNumList.size(); j++) {
-                    int checkComputerNum = computerNumList.get(j);
-                    if (checkComputerNum == checkUserNum) {
-                        if (i == j) {
-                            strike++;
-                        } else {
-                            ball++;
-                        }
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            if (strike == 3) {
-                System.out.println("정답입니다.");
-                System.out.println(x+"번만에 맞히셨습니다!");
-                System.out.println("게임을 종료합니다.");
-                break replay;
-            } else {
-                System.out.println(ball + "B" + strike + "S");
-                continue replay;
-            }
-        }
     }
 }
 
@@ -228,3 +183,18 @@ public class Baseball {
 //        System.out.println("컴퓨터가 숫자 생성 완료! 답을 맞춰보세요!");
 // 유저가 10이상의 수를 입력하거나 정수가 아닌 다른 타입을 입력하는 경우 예외 처리를 해줘야 함
 // -> 10이상의 정수는 if 문으로 간단하게 처리 할 수 있지만 다른 타입은 컴파일에러가 발생 -> 따로 예외 처리를 해줘야 하는데 생각이 안남 --> 4주차 학습 후에 다시 시도
+
+// class 를 나눈뒤 메서드를 사용해서 main 을 깔끔하게 사용하고싶음, 바꾸는 김에 자리수도 추가하고싶음!
+// 1. class main - 생성할 자리수 입력, 시도한 횟수 체크
+// - 1. Scanner 로 자리수 받기 selectNum
+// - 2. 자리수로 난수생성.method 실행
+// - 3. for 문 안에서 유저입력.method, 비교하기.method 반복 횟수를 try 에 입력받아서 체크
+// - 4. 기타 문구 출력
+// 2. class 난수생성 - method1 난수생성 및 중복검사
+// - 1. 생성자에 매개변수 selectNum 을 받고 this. 을 이용해서 클래스에 자리수 저장
+// - 2. selecNum 만큼 난수생성, 만들어 놓은 코드 일부수정!
+// 3. class 유저입력 - method1 유저입력 및 오류검사
+//   3.1 아직 4주차를 못들어서 오류를 예외처리 하지않고, 입력을 String 으로 받은 뒤 강제형변환을 하려했으나 마찬가지 일듯! -> 예외처리를 받아야함
+// - 1,2. 난수생성과 동일!
+// 4. class 비교하기 - method1 난수와 입력값 비교
+// - 1,2 난수생성과 동일!
